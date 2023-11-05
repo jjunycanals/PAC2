f2b2675f617092dae78a0f0e11f487a896fe44fb
Jordi Juny <jjunyc@uoc.edu>
2023-11-05 15:51:33 +0100

    ex 3 apartats d i e (every i some)

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -2,14 +2,20 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every((number) => number % 2 === 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  var primertipo = typeof(input[0]);
+  for (let i = 0; i < input.length; i++) {
+    if (typeof input[i] !== primertipo) {
+      return false;
+    }    
+  }
+  return true;
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +23,43 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  if (!Array.isArray(input) || input.length === 0) {
+    return false; // Comprovo si es una matriu valida (array de arrays).
+  }
+
+  for (const fila of input) {
+    if (!Array.isArray(fila) || fila.length === 0) {
+      return false; // Comprovo que cada element es un array.
+    }
+
+    for (const valor of fila) {
+      if (typeof valor !== 'number' || valor <= 0) {
+        return false; // Miro si cada elelment del array es un nombre més gran de 0.
+      }
+    }
+  }
+  return true;
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  var resultat = input.map((lletra) => {
+    return (lletra.split('').map((char) => {
+      return char.match(/[aeiou]/ig)
+    })).join('')
+  });
+
+  for (const vocal of resultat) {
+    var lletres = vocal.split('');
+    for (let i = 0; i < lletres.length; i++) {
+      if (lletres[0]!==lletres[i]) {
+        return false;
+      }     
+    }
+  }
+  return true;
 }
 
 module.exports = {
--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,28 +2,56 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  for (const element of input) {
+    if (typeof element === 'number' && element > 10) {
+      return true; // Si cada numero es més gran que 10, retorno true.
+    }
+  }
+  return false; // If no number is greater than 10, return false.
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  for (const element of input) {
+    if (typeof element === 'string' && element.length > 10) {
+      return true; // Si cada string es més gran que 10 caracteres, retorno true.
+    }
+  }
+  return false;
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  for (const row of input) {
+    if (!Array.isArray(row) || row.length === 0) {
+      return false; // Reviso si cada element es un array
+    }
+
+    for (const element of row) {
+      if (element === true) {
+        return true; // Si algun element es true, retorno true.
+      }
+    }
+  }
+  return false;
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  for (let i = 0; i < input.length; i++) {
+    const paraula = input[i].split(' ');
+    var lost = paraula.some((x) => x.toLowerCase() === 'lost');
+    if (lost !== false){
+      return true;
+    }
+  }
+  return false;
 };
 
 module.exports = {
